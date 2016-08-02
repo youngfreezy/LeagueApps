7b8f536c10cdebc2aa49098a4ada930ede4f16ae
Fareez Ahmed <fareez.ahmed@ge.com>
2016-08-02 10:57:04 -0400

    gitignore apikey

--- public/app.js
+++ public/app.js
@@ -1,3 +1,1 @@
-var leagueScheduleApp = angular.module('leagueScheduleApp', ['ngAnimate']).constant("myConfig", {
-  "apiKey": "34e705149fde3469fe3bccda7d085049"
-})
+var leagueScheduleApp = angular.module('leagueScheduleApp', ['ngAnimate']);
